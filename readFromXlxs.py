50a934caf7bb486b05a5a03de200592a1e9b398c
MoazAMoshtha <mama.moshtha@gmail.com>
2021-02-10 18:56:09 +0200

    Update readFromXlxs.py

--- readFromXlxs.py
+++ readFromXlxs.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from openpyxl import load_workbook
 
 count = 1
-file_name = '60000_1.xlsx'
+file_name = '100000.xlsx'
 path = r'C:\Users\Laptop\Desktop\\' + file_name
 
 if __name__ == '__main__':
@@ -12,13 +12,17 @@ if __name__ == '__main__':
     web = webdriver.Firefox()
     web.get("https://www.elections.ps/tabid/596/language/ar-PS/Default.aspx")
     sheet = excel_document['ورقة1']
-    user_info = []
-    for i in range(1376, sheet.max_row + 1):
+    sleepAfter = 1
+    for i in range(1, sheet.max_row + 1):
+        if sleepAfter > 20:
+            time.sleep(30)
+            sleepAfter = 1
+        sleepAfter += 1
         a = 'A' + str(i)
         b = 'B' + str(i)
         c = 'C' + str(i)
         id = sheet[a].value
-        yob=sheet[b].value
+        yob = sheet[b].value
 
         web.find_element_by_id("dnn_ctr4525_View_PalID").send_keys(id)
         web.find_element_by_id("dnn_ctr4525_View_YearOfBirth").send_keys(yob)
